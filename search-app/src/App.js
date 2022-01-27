410e0a636e23832f052ba71fbc99773e2e945e21
Bret Kitchell <brittonashford@gmail.com>
2022-01-26 21:26:37 -0600

    simple app built with Create React App

---
+++ search-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Container, Carousel } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Carousel>
+          <Container>
+            <h1>Search App</h1>
+            <p>This is a simple search app</p>
+
+            <SearchForm />
+          </Container>
+        </Carousel>
+        <Results />
+      </div>
+    );
+  }
+}
+export default App;
